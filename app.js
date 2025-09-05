f446753f673fc944f397c73d1c20af0236a0529e
Aditya Pant <adityapcodes@gmail.com>
2025-09-05 20:32:32 +0530

    cors resolve

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./db');
+const cors = require('cors');
+
+app.use(cors({
+  origin: ['https://notes-app-black-kappa.vercel.app', 'http://localhost:5173'], // Add localhost port if developing locally
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 const app = express();
 
